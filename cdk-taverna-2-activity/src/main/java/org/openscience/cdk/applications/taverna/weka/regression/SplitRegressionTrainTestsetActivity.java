3fb741d8bd8b90509f5a67c49fa4f0f6d9161687
andreas1981 <andreas1981@e522cf0d-97f0-4144-ada5-e1fc469fca11>
2011-04-16 09:41:02 +0000

    - New upload of the regression folder
    git-svn-id: https://cdktaverna2.svn.sourceforge.net/svnroot/cdktaverna2/trunk@316 e522cf0d-97f0-4144-ada5-e1fc469fca11

---
+++ cdk-taverna-2-activity/src/main/java/org/openscience/cdk/applications/taverna/weka/regression/SplitRegressionTrainTestsetActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010 - 2011 by Andreas Truszkowski <ATruszkowski@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package org.openscience.cdk.applications.taverna.weka.regression;
+
+import org.openscience.cdk.applications.taverna.CDKTavernaConstants;
+import org.openscience.cdk.applications.taverna.weka.utilities.AbstractSplitTrainTestsetActivity;
+
+/**
+ * Class which represents the split dataset into train/testset activity.
+ * 
+ * @author Andreas Truzskowski
+ * 
+ */
+public class SplitRegressionTrainTestsetActivity extends AbstractSplitTrainTestsetActivity {
+
+	/**
+	 * Creates a new instance.
+	 */
+	public SplitRegressionTrainTestsetActivity() {
+		this.INPUT_PORTS = new String[] { "Weka Regression Dataset" };
+		this.OUTPUT_PORTS = new String[] { "Regression Train Datasets", "Regression Test Datasets" };
+	}
+
+	@Override
+	public String getFolderName() {
+		return CDKTavernaConstants.WEKA_REGRESSION_FOLDER_NAME;
+	}
+
+}
